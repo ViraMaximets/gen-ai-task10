f261ede847dd748a76738e846e89700e3c70071e
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2023-09-04 17:05:22 +0300

    UC#10 Implement regex

--- src/main/java/com/gen/ai/Main.java
+++ src/main/java/com/gen/ai/Main.java
@@ -1,7 +1,27 @@
 package com.gen.ai;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(validatePassword("Pssw0//rd", 12));
+    }
+
+    public static boolean validatePassword(String input, int maxLength) {
+        // validate the maximum string length
+        if (input.length() > maxLength) {
+            return false;
+        }
+
+        // uppercase, lowercase letter, digit, special character
+        if (!Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]).*$", input)) {
+            return false;
+        }
+
+        // not contain any whitespace characters
+        if (Pattern.matches(".*\\s.*", input)) {
+            return false;
+        }
+        return true;
     }
 }
